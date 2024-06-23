1ab2e6c13bd2759e07ee374d32e0c8c5a0e48487
Bernard0101 <bernardogeocometto@gmail.com>
2024-06-23 12:50:26 -0300

    correcoes nas partes do treinamento e de calculo da perda, inicio do aprendizado do perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,63 +1,81 @@
 import numpy as np
 import random
 
+#initialize the perceptron
 class Perceptron():
-    def __init__(self, learning_rate=0.01, data_length=int, preds=int):
+    def __init__(self, learning_rate=0.1, n_features=2):
+        self.n_features=n_features
         self.learning_rate=learning_rate
-        self.loss=np.random.rand(data_length)
-        self.weights=np.random.rand(data_length)
-        self.data=np.arange(0, data_length)
-        if preds is not None:
-            self.predictions=preds
-        else:
-            self.predictions=np.random.rand(data_length)
-        self.bias=random.randint(0, 10)
+        self.weights=np.random.rand(n_features)
+        self.bias=random.uniform(0, 1)
 
         
-    #aplica uma funcao ax+b em todos as predicoes do modelo
-    def fit(self):
+    #apply a linear function to all parameters
+    def fit(self, features):
         Z=0
-        preds=[]
-        for index, x in enumerate(self.weights):
-            Z+=x*self.predictions[index]
-            preds.append(x)
-        Z+=self.bias
-        return Z, preds
+        for f in features:
+            for x in self.weights:
+                Z+= x * f
+            Z+=self.bias
+            return Z
 
-    #usa a funcao degrau, a ativacao mais simples
+    #ladder activation funtion
     def activation_function(self, Z):
-        if Z <= 0:
-            prediction = 0
-            return prediction
-        else: 
-            prediction = 1
-            return prediction
-        
-    #calculate the loss for each point in the data based on the given prediction
-    def Calculate_Loss(self):
-        total_loss=0
-        for index, i in enumerate(self.data):
-            loss=i-self.predictions[index]
-            self.loss[index]=loss
-            total_loss += loss
-        mean_loss=total_loss / len(self.loss)
-        return mean_loss
+        return 1 if Z > 1 else 0
     
-    #optimize the model parameters/weights
-    def Optimize_model_parameters(self, mean_loss):
-        for index, weight in enumerate(self.weights):
-            self.weights[index]= weight + (self.learning_rate * self.loss[index]*self.data[index])
-        self.bias=self.bias + (self.learning_rate * mean_loss)
-        return mean_loss
+    #calculate the error for that specific data
+    def Calculate_error(self, prediction):
+        return labels-prediction
+
+
+
+#calculate the loss for each point in the data based on the given prediction
+def Calculate_Loss_MSE(loss):
+    MSE_error=0
+    for index, error in enumerate(loss):
+        MSE_error+=(error**2)
+    MSE_error=MSE_error/len(loss)
+    return MSE_error
+
+                    
+
+#creates the dataset to predict the logic gate and
+X=np.array([
+           [0,0], 
+           [1,0],
+           [0,1],
+           [1,1]])
+
+y=np.array([0, 0, 0, 1])
+    
 
 #crete a training loop for the model 
-preds=None
+loss=[]
 epochs=100
-for epoch in range(epochs):
-    model_percetron=Perceptron(data_length=10, preds=preds)
-    Z, preds=model_percetron.fit()
-    prediction=model_percetron.activation_function(Z)
-    mean_loss=model_percetron.Calculate_Loss()
-    model_percetron.Optimize_model_parameters(mean_loss)
-    if epoch % 10 == 0:
-     print(f"epoch: {epoch} mean loss: {mean_loss} ")
+for features, labels in zip(X, y):
+    for epoch in range(epochs):
+        perceptron=Perceptron(learning_rate=0.2, n_features=2)
+
+        #apply linear function (ax+b) and an activation funtion (ladder)
+        Z=perceptron.fit(features) 
+        prediction=perceptron.activation_function(Z)
+
+        #calculate the loss using (MSE)
+        error=perceptron.Calculate_error(prediction)
+        loss.append(error)
+        MSE_loss=Calculate_Loss_MSE(loss)
+
+        #optimize model parameters
+        perceptron.weights += perceptron.learning_rate * error * features
+        perceptron.bias += perceptron.learning_rate * error
+
+        if epoch % 10 == 0:
+            print(f"Epoch {epoch}, Mean Loss: {MSE_loss:.4f}")
+
+        
+
+
+
+
+
+
