66b73adb759c4f4d040ac1e522538ddcffd8d11c
Bernard0101 <bernardogeocometto@gmail.com>
2024-07-04 14:08:52 -0300

    initial construction of the MLP

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -1,6 +1,26 @@
+import numpy as np
+from perceptron import Perceptron, TrainPerceptron
+features = None
 
-from perceptron import Perceptron
+#input layer perceptrons
+p=Perceptron(n_features=len(features))
+node=TrainPerceptron()
 
-class Multi_layer_perceptron(Perceptron):
-    def __init__(self):
+
+class MultiLayerPerceptron(Perceptron, TrainPerceptron):
+    def __init__(self, hidden_layers=0):
         pass
+
+    def input_layer(self):
+        feed1=node.training_loop(p)
+        feed2=node.training_loop(p)
+    
+    def hidden_layer(self, feed1, feed2):
+        pred1 = feed1
+        pred2 = feed2
+        p1=p.fit(X=feed1)
+        p2=p.fit(X=feed2)
+        node.training_loop(p1)
+        node.training_loop(p2)
+        
+
--- perceptron.py
+++ perceptron.py
@@ -1,88 +1,78 @@
 import numpy as np
-import random
 
-#initialize the perceptron
-class Perceptron():
-    def __init__(self, learning_rate=0.1, n_features=2):
-        self.n_features=n_features
-        self.learning_rate=learning_rate
-        self.weights=np.random.rand(n_features)
-        self.bias=random.uniform(0, 1)
 
-        
-    #apply a linear function to all parameters
-    def fit(self, features):
-        Z=0
-        for f in features:
-            for x in self.weights:
-                Z+= x * f
-            Z+=self.bias
-            return Z
+def activation_ReLU(Z):
+    return Z if Z >= 0 else 0
 
-    #ladder activation funtion
-    def activation_function(self, Z):
-        return 1 if Z > 0 else 0
+def calculate_MSE(losses):
+    mse_error=0
+    for index, error in enumerate(losses):
+        mse_error+= error**2
+    mse_error/=len(losses)
+    return mse_error
+
+class Percetron:
+    def __init__(self, learning_rate=0.001, n_features=0):
+        self.features= n_features
+        self.learning_rate= learning_rate
+        self.weights= np.random.rand(n_features)
+        self.loss= np.random.rand(n_features)
+        self.bias= np.random.randint(2)
+
+    #aplica funcao de dot product a usa funcao de ativacao ReLU
+    def fit(self, X, Z=0):
+        for weight in (self.weights):
+            for x in X:
+                Z+= weight * x
+        Z+=self.bias
+        prediction=activation_ReLU(Z)
+        return prediction
     
-    #calculate the error for that specific data
-    def Calculate_error(self, prediction):
-        return labels-prediction
+    #calculate the error for that specific prediction
+    def calculate_error(self, prediction, label):
+        loss= label-prediction
+        return loss
 
 
+features=np.array([[0,0],
+                   [0,1],
+                   [1,0],
+                   [1,1]])
 
-#calculate the loss for each point in the data based on the given prediction
-def Calculate_Loss_MSE(epoch_errors):
-    MSE_error=0
-    for index, error in enumerate(epoch_errors):
-        MSE_error+=(error**2)
-    MSE_error=MSE_error/len(epoch_errors)
-    return MSE_error
-
-                    
-#creates the dataset to predict the logic gate AND
-X=np.array([
-           [0,0], 
-           [1,0],
-           [0,1],
-           [1,1]])
-
-y=np.array([0, 0, 0, 1])
-    
+labels=np.array([0,0,0,1])
 
 
-#instantiate the model 
-perceptron=Perceptron(learning_rate=0.2, n_features=2)
-
-#crete a training loop for the model 
-loss=[]
 epochs=100
-#define the number of epochs 
-for epoch in range(epochs):
-    epoch_errors=[]
-    for features, labels in zip(X, y):
+num_features=len(features)
+p1 = Percetron(n_features = num_features)
 
-        #apply linear function (ax+b) and an activation funtion (ladder)
-        Z=perceptron.fit(features) 
-        prediction=perceptron.activation_function(Z)
+class TrainPerceptron:
+    def __init__(self, model):
+        self.model= model
+        self.predictions=[]
 
-        #calculate the error of the perceptron and add to the errors list
-        error=perceptron.Calculate_error(prediction)
-        epoch_errors.append(error)
+    def training_loop(self):
+        for epoch in range(epochs):
+            losses=[]
+            for X, y in zip(features, labels):
 
-        #optimize model parameters
-        perceptron.weights += perceptron.learning_rate * error * features
-        perceptron.bias += perceptron.learning_rate * error
+                #calculate the sum product and do the activation function
+                prediction=self.model.fit(X)
+                self.predictions.append(prediction)
 
-    #calculate the loss on each epoch using (MSE) algorithm
-    MSE_loss=perceptron.Calculate_error(prediction)
-    MSE_loss=Calculate_Loss_MSE(epoch_errors)
+                #calculate the error of that prediction 
+                loss=self.model.calculate_error(prediction, label=y)
+                losses.append(loss)
+
+                #optimize model parameters
+                for x in X:
+                    self.model.weights += self.model.learning_rate * loss * x
+                self.model.bias += self.model.learning_rate * loss
 
     
-    if epoch % 10 == 0:
-        print(f"Epoch {epoch}, MSE Loss: {MSE_loss:.4f}")
+    def predict(self):
+        print(self.predictions)
+        return self.predictions
 
-
-
-
-
-
-
+train_perceptron= TrainPerceptron(model=p1)
+preds=train_perceptron.training_loop()
