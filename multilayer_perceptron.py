58b2b7c9ce6b4cd59f682997037e4d8b443a7a08
Bernard0101 <bernardogeocometto@gmail.com>
2024-07-19 11:25:04 -0300

    initial neural network skecth yet without backpropagation

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import numpy as np
 from perceptron import Perceptron, TrainPerceptron
 features = None
@@ -24,3 +25,86 @@ class MultiLayerPerceptron(Perceptron, TrainPerceptron):
         node.training_loop(p2)
         
 
+=======
+from perceptron import Perceptron, TrainPerceptron
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv('pythons/Perceptron_implementation-main/salary_data_with_noise.csv')
+features = data[['Age', 'Education']].values
+labels = data['Salary'].values
+
+
+p=Perceptron(learning_rate=00.1, n_features=len(features))
+pt=TrainPerceptron(p, features=features, labels=labels)
+
+def activation_leaky_ReLU(z, alpha=0.01):
+    return np.where(z >= 0, z, alpha * z)
+
+def calculate_MSE(losses, mse_error=0):
+    for index, error in enumerate(losses):
+        mse_error+= error**2
+    mse_error/=len(losses)
+    return mse_error
+
+class MultilayerPerceptron():
+    def __init__(self):
+        self.weights=np.random.rand()
+        self.loss=np.random.rand()
+
+    #the initial input layer with two nodes each with weights eqauling to the number 
+    #of features needed to be predicted and outputing two number that will feed the next layer
+    def input_layer(self, out_features=[]):
+        pred1=pt.training_loop()
+        pred2=pt.training_loop()
+        out_features.append(pred1, pred2)
+        return out_features
+
+    #first hidden layer, with inputs of the previous two layers, outputing 
+    def hidden_layer(self, predictions, hidden_nodes, new_preds=[]):
+        self.weights=np.random.rand(len(predictions))
+        self.bias=np.random.randint(7)
+        for node in range(hidden_nodes):
+            Z=0
+            for index, preds in enumerate(predictions):
+                Z+=preds*self.weights[index]
+            Z+=self.bias
+            new_preds.append(Z)
+        return new_preds
+    
+    #the activation layer of the neural network
+    def activation_layer(self, new_preds, predictions=[]):
+        bias=np.random.randint(7)
+        for preds in new_preds:
+            predictions.append(activation_leaky_ReLU(preds))
+        return predictions
+    
+    def CalculateLoss(self, predictions, errors=[], mse_loss=0):
+        for label, pred in zip(labels, predictions):
+            error=pred-label
+            errors.append(error)
+        mse_loss=calculate_MSE(losses=errors)
+        return mse_loss
+    
+    #def Backpropagation(self):
+
+
+nn=MultilayerPerceptron()
+
+class TrainMLP():
+    def __init__(self, epochs=100):
+        self.epochs=epochs
+    
+    def training(self, losses=[]):
+        for epoch in range(self.epochs):
+           in_features=nn.input_layer()
+           in_hidden_features=nn.hidden_layer(predictions=in_features, hidden_nodes=4)
+           in_hidden_features=nn.hidden_layer(predictions=in_hidden_features, hidden_nodes=4)
+           in_activation_features=nn.activation_layer(new_preds=in_hidden_features)
+           mse_Loss=nn.CalculateLoss(predictions=in_activation_features)
+
+           losses.append(mse_Loss)
+
+
+        
+>>>>>>> c38ecb4 (initial neural network skecth yet without backpropagation)
--- perceptron.py
+++ perceptron.py
@@ -1,6 +1,9 @@
 import numpy as np
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> c38ecb4 (initial neural network skecth yet without backpropagation)
 def activation_ReLU(Z):
     return Z if Z >= 0 else 0
 
@@ -11,7 +14,11 @@ def calculate_MSE(losses):
     mse_error/=len(losses)
     return mse_error
 
+<<<<<<< HEAD
 class Percetron:
+=======
+class Perceptron:
+>>>>>>> c38ecb4 (initial neural network skecth yet without backpropagation)
     def __init__(self, learning_rate=0.001, n_features=0):
         self.features= n_features
         self.learning_rate= learning_rate
@@ -19,6 +26,12 @@ class Percetron:
         self.loss= np.random.rand(n_features)
         self.bias= np.random.randint(2)
 
+<<<<<<< HEAD
+=======
+    def activation_ReLU(Z):
+        return Z if Z >= 0 else 0
+
+>>>>>>> c38ecb4 (initial neural network skecth yet without backpropagation)
     #aplica funcao de dot product a usa funcao de ativacao ReLU
     def fit(self, X, Z=0):
         for weight in (self.weights):
@@ -44,17 +57,32 @@ labels=np.array([0,0,0,1])
 
 epochs=100
 num_features=len(features)
+<<<<<<< HEAD
 p1 = Percetron(n_features = num_features)
 
 class TrainPerceptron:
     def __init__(self, model):
         self.model= model
+=======
+p1 = Perceptron(n_features = num_features)
+
+class TrainPerceptron:
+    def __init__(self, model, features, labels):
+        self.features=features
+        self.labels=labels
+        self.model=model
+        self.losses=[]
+>>>>>>> c38ecb4 (initial neural network skecth yet without backpropagation)
         self.predictions=[]
 
     def training_loop(self):
         for epoch in range(epochs):
             losses=[]
+<<<<<<< HEAD
             for X, y in zip(features, labels):
+=======
+            for X, y in zip(self.features, self.labels):
+>>>>>>> c38ecb4 (initial neural network skecth yet without backpropagation)
 
                 #calculate the sum product and do the activation function
                 prediction=self.model.fit(X)
@@ -62,12 +90,17 @@ class TrainPerceptron:
 
                 #calculate the error of that prediction 
                 loss=self.model.calculate_error(prediction, label=y)
+<<<<<<< HEAD
                 losses.append(loss)
+=======
+                self.losses.append(loss)
+>>>>>>> c38ecb4 (initial neural network skecth yet without backpropagation)
 
                 #optimize model parameters
                 for x in X:
                     self.model.weights += self.model.learning_rate * loss * x
                 self.model.bias += self.model.learning_rate * loss
+<<<<<<< HEAD
 
     
     def predict(self):
@@ -76,3 +109,18 @@ class TrainPerceptron:
 
 train_perceptron= TrainPerceptron(model=p1)
 preds=train_perceptron.training_loop()
+=======
+        return prediction
+
+    
+    #def predict(self, preds):
+        #print(preds)
+        #print(self.predictions)
+        #print(self.losses)
+        #return self.predictions
+
+train_perceptron= TrainPerceptron(model=p1, features=features, labels=labels)
+preds=train_perceptron.training_loop()
+#train_perceptron.predict(preds)
+
+>>>>>>> c38ecb4 (initial neural network skecth yet without backpropagation)
