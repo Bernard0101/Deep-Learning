dea7cfcbb437332613f7591e9786c2ba10a2cd1e
Bernard0101 <bernardogeocometto@gmail.com>
2024-08-03 19:39:12 -0300

    implementation of the forward method

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -2,6 +2,7 @@ from perceptron import Perceptron, TrainPerceptron
 import numpy as np
 import pandas as pd
 
+#the dataset and it`s preprocessing
 data = pd.read_csv('salary_data_with_noise.csv')
 features = data[['Age', 'Education']].values
 labels = data['Salary'].values
@@ -12,24 +13,28 @@ p=Perceptron(learning_rate=00.1, n_features=features.shape[1])
 #perceptron training objects
 pt=TrainPerceptron(model=p, features=features, labels=labels)
 
+
+def activation_leaky_ReLU_derivative(Z, alpha=0.01):
+    return alpha if Z < 0 else 1
+
 def activation_leaky_ReLU(z, alpha=0.01):
     return np.where(z >= 0, z, alpha * z)
 
-def MSE_Loss_derivative(alpha=0.01):
-    2*()
+def MSE_Loss_derivative(y_pred, y_label):
+    return 2*(y_pred-y_label)
 
-def calculate_MSE(losses, mse_error=0):
-    for index, error in enumerate(losses):
+def MSE_Loss(losses, mse_error=0):
+    for  error in losses:
         mse_error+= error**2
     mse_error/=len(losses)
     return mse_error
 
 class MultilayerPerceptron():
-    def __init__(self, n_features=len(features), input_nodes=2, hidden_layers=2, hidden_nodes=3):
+    def __init__(self, n_features=len(features), input_nodes=2, hidden_layers=2, hidden_nodes=6):
         self.input_nodes=input_nodes
         self.hidden_layers=hidden_layers
         self.input_weights=np.random.rand(n_features, input_nodes)
-        self.hidden_weights=[np.random.rand(input_nodes if i == 0 else hidden_nodes, hidden_nodes) for i in range(hidden_layers)]
+        self.hidden_weights=[np.random.rand(input_nodes if i == 0 else hidden_nodes, hidden_nodes) for i in range(hidden_layers-1)]
         self.bias=np.random.randint(10)
 
 
@@ -47,7 +52,7 @@ class MultilayerPerceptron():
             Z=0
             for index, preds in enumerate(predictions):
                 
-                Z+=preds*self.hidden_weights[layer][index, node]
+                Z+=preds*self.hidden_weights[0][:, node]
             Z+=self.bias
             out_hidden_features.append(Z)
         out_hidden_features=np.squeeze(out_hidden_features)
@@ -62,15 +67,31 @@ class MultilayerPerceptron():
     
 
     #the calculation of the mse loss 
-    def CalculateLoss(self, predictions, errors=[], mse_loss=0):
+    def CalculateLoss(self, predictions, errors=[], mse_Loss=0):
         for label, pred in zip(labels, predictions):
             error=pred-label
             errors.append(error)
-        mse_loss=calculate_MSE(losses=errors)
-        return mse_loss
+        mse_Loss=MSE_Loss(losses=errors)
+        return mse_Loss
 
-    #the backpropagation algorithm and its gradient descent calculus
- #   def Backpropagation(self, predictions):
+
+    def Forward(self):
+        #the initial input layer 
+        in_features=nn.input_layer(input_nodes=nn.input_nodes)
+
+        #all the hidden layers and also its activation functions
+        in_features=nn.hidden_layer(predictions=in_features, hidden_nodes=3, layer=0)
+        in_features=nn.hidden_layer(predictions=in_features, hidden_nodes=3, layer=1)
+        in_activation_features=nn.activation_layer(out_hidden_features=in_features)
+        return in_activation_features
+
+
+
+
+
+    #the backpropagation algorithm and its gradient descent calcculation
+    def Backpropagation(self, predictions):
+        MSE_Loss_derivative(y_pred=predictions, y_label=labels)
         
 
 
@@ -81,28 +102,19 @@ class MultilayerPerceptron():
 nn=MultilayerPerceptron(input_nodes=2, hidden_layers=2, hidden_nodes=3)
 print(nn.hidden_weights)
 
-
 class TrainMLP():
     def __init__(self, model, epochs=10):
         self.model=model
         self.epochs=epochs
     
     def training(self, losses=[]):
-        #for epoch in range(self.epochs):
-
-            #returns the array with 10 lists
-            in_features=nn.input_layer(input_nodes=nn.input_nodes)
-           # print(in_features)
-
-
-            #receives those in_features that are an array and passes it to the next layer trough all of the nodes in it
-            in_features=nn.hidden_layer(predictions=in_features, hidden_nodes=3, layer=0)
-            in_features=nn.hidden_layer(predictions=in_features, hidden_nodes=3, layer=1)
-
+        for epoch in range(self.epochs):
 
             #receives the predictions from the hidden layers and activate using the activation function
-            in_activation_features=nn.activation_layer(out_hidden_features=in_features)
-            mse_Loss=nn.CalculateLoss(predictions=in_activation_features)
+            y_pred=nn.Forward()
+            mse_Loss=nn.CalculateLoss(predictions=y_pred)
+            #nn.Backpropagation(predictions=y_pred)
+
 
 nn_train=TrainMLP(epochs=10, model=nn)
 nn_train.training()
